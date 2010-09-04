ed9828cea819f6e3ccfda93c13f68715ad08cf02
Jen Smith <jennifer.e.m.smith@gmail.com>
2010-09-04 02:14:23 +0100

    Added better data

--- myapp.rb
+++ myapp.rb
@@ -2,19 +2,20 @@ require 'rubygems'
 require 'sinatra'
 require 'json'
 
-def create_item name, image
-    {:name => name,:imagePath=>image}
+def create_item id, name, image
+    {:name => name, :id => id , :imagePath=>image}
 end
 
 def create_items
     result = [
-        create_item("foo", "images/redcar.png"),
-        create_item("bar","images/redcar.png"),
-        create_item("baz","images/redcar.png"),
-        create_item("john","images/redcar.png"),
-        create_item("paul","images/redcar.png"),
-        create_item("george","images/redcar.png"),
-        create_item("ringo","images/redcar.png")
+        create_item("stobart","Eddie Stobart", "http://upload.wikimedia.org/wikipedia/en/7/74/Eddie_Stobart_logo.png"),
+        create_item("salveson","Christian Salveson","http://upload.wikimedia.org/wikipedia/en/f/f0/Salvesen.png"),
+        create_item("maersk","Maersk","http://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Maersk_logo.svg/125px-Maersk_logo.svg.png"),
+        create_item("rankhovis","Rank Hovis","http://www.rankhovis.co.uk/images/logo.gif"),
+        create_item("sainsburies","Sainsburies","http://www.sainsburys.co.uk/support_files/style_images/common/header/header-logo.gif"),
+        create_item("tescos","Tescos","http://www.jutexpo.co.uk/IMAGES/Tesco_logo.png"),
+        create_item("warburtons","Warburtons","http://www.eurowaxpack.org/files/pictures%20logos/Warburtons-Logo.jpg"),
+        create_item("royalmail","Royal Mail","http://www.royalmail.com/images/royalmail/paarch/pa_rmlogo.gif")
         ];
 end
 
--- www/gamescreen.js
+++ www/gamescreen.js
@@ -9,33 +9,34 @@ Convoy.views.GameScreen = Ext.extend(Ext.Panel, {
         pack  : 'start'
     },
     initComponent: function() {
-        var mainPanel = new Convoy.views.MainPanel({
-            width: '75%'
-        });
+
         var tpl = new Ext.XTemplate(
             '<tpl for=".">',
-                '<div class="item-box" id="{id}" style=" background-image:url({imagePath})">{name}<br/>',
+                '<div class="item-box"  >',
+                        '<div style=" background-image:url({imagePath})"><p>{name}</p></div>',
                 '</div>',
             '</tpl>',
             '<div class="x-clear"></div>'
         );
-        var store = Convoy.CreateTrucksReader();
-        
+        this.store = Convoy.CreateTrucksReader();
 
+
+        var dataView = new Ext.DataView({
+            store: this.store,
+            tpl: tpl,
+            overClass:'x-view-over',
+            itemSelector:'div.item-box',
+            emptyText: 'No data',
+            scroll:true ,
+            singleSelect:false,
+            multiSelect:false
+        });
         var panel = new Ext.Panel({
-            id:'images-view',
-           
+            id:'main-view',
+            width: '75%',
 
-            items: new Ext.DataView({
-                store: store,
-                tpl: tpl,
-                overClass:'x-view-over',
-                itemSelector:'div.item-box',
-                emptyText: 'No data',
-                scroll:true ,
-                singleSelect:false,
-                multiSelect:false
-            })
+
+            items: dataView
         });
 
         var rightPanel = new Ext.Panel({
@@ -43,81 +44,16 @@ Convoy.views.GameScreen = Ext.extend(Ext.Panel, {
             width: '25%',
             items:[panel]
         });
-		this.items =panel;//[mainPanel, rightPanel];
+		this.items =[panel, rightPanel];
 
         Convoy.views.StartScreen.superclass.initComponent.call(this);
-        store.load();
+        dataView.on("itemtap", this.itemTapped, this);
+        this.store.load();
 
 
-        Trucks.get(function(trucks){
-            for(var i = 0; i < trucks.length; i++){
-              //  mainPanel.addItem(trucks[i]);
-                
-            }
-        });
-	}
+	}     ,
+    itemTapped: function(dataView, index, item, e){
+        alert("you just clicked on " + item["id"]);
+        
+    }
 });
-
-Convoy.views.MainPanelRow = Ext.extend(Ext.Panel,{
-    defaults:{
-        height:200,
-        width:320 ,
-        baseCls: "item-box",
-        xtype : "panel"
-    },
-    layout: 'hbox',
-    layoutConfig: {
-        align: 'top'
-    },
-    initComponent: function() {
-        Convoy.views.MainPanel.superclass.initComponent.call(this);
-	},
-    addItem:function(item){
-
-      this.add({html:item.name});
-      this.doLayout();
-    }
-  }
-
-);
-
-//Ext.layout.TYPES['main-panel-row'] = Convoy.views.MainPanelRow; 
-Ext.reg('main-panel-row', Convoy.views.MainPanelRow);
-
-
-Convoy.views.MainPanel = Ext.extend(Ext.Panel,{
-   defaults:{
-        xtype : "main-panel-row"
-    },
-    columns: 2,
-    layout: 'vbox',
-    layoutConfig: {
-        align: 'top'
-    },
-    initComponent: function() {
-        Convoy.views.MainPanel.superclass.initComponent.call(this);
-	},
-    addRow : function(){
-        var that = this;
-        this.add({rowNum:1, xtype:"main-panel-row", html:"foo"});
-        this.add({rowNum:1, xtype:"button", handler: function(){
-            alert(that.items[0]);
-        }});
-        this.doLayout();
-    } ,
-    currentRow : function(){
-        if(this.items.length==0){
-           this.addRow();
-        };
-        return this.items[0];
-    },
-    addItem:function(item){
-this.addRow();
-//      if (this.currentRow().items.length == columns){
-//          this.addRow();
-//      }
-      //this.currentRow().addItem(item);
-    }
-  }
-
-);
